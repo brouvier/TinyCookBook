edd974056a256a548c562fd811a79572bb118dc8
Baptiste ROUVIER <brouvier@gmail.com>
2022-11-21 11:13:49 +0100

    Migration vers une BdD

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from "rxjs";
 import { MessageService } from "primeng/api";
+import { environment } from "src/environments/environment";
+import { SecurityService } from "./security.service";
 
 
 export interface Recipe {
@@ -9,83 +11,96 @@ export interface Recipe {
     name: string, path: string,
     imgPath?: string, comment?: string, keywords?: string,
 
+    error?: string,
     display?: boolean, filterKeyWorkds?: string
 }
 
-export const EMPTY_RECIPE: Recipe = {
-    name: "",
-    path: ""
+interface ApiResponse {
+    success?: { code: number, status: string },
+    error?: { code: number, status: string }
 }
+
+export const EMPTY_RECIPE: Recipe = { name: "", path: "" }
 
 @Injectable()
 export class RecipeService {
 
-    private recipes: Recipe[] = [];
+    readonly apiRecipePath = environment.apiLocation + 'recipe/';
+
     private recipesSubject$ = new BehaviorSubject<Recipe[]>([]);
 
-
-    constructor(private httpClient: HttpClient, private messageServ: MessageService) {
-        this.httpClient.get<Recipe[]>(
-            "assets/recipe.json"
-        ).subscribe({
-            next: (response) => {
-                this.recipes = response;
-                this.sendRecipe();
-            },
-            error: (e) => console.log('Erreur : ', e)
-        });
-    }
-
-    /** 
-     * Expose une copie de la liste des recettes
-     */
-    private sendRecipe() {
-        this.recipes.sort((a: Recipe, b: Recipe) => {
-            if (a.name < b.name) return -1;
-            if (a.name > b.name) return 1;
-            return 0;
-        });
-        const recipes = [...this.recipes];
-        recipes.forEach(r => {
-            r.display = false;
-            r.filterKeyWorkds = r.name + " " + r.keywords;
-        });
-        this.recipesSubject$.next(recipes);
-    }
-
-    public getRecipes(): Observable<Recipe[]> {
-        return this.recipesSubject$.asObservable();
-    }
-
-    public addRecipe(recipe: Recipe) {
-        this.recipes.push(recipe);
+    constructor(private httpClient: HttpClient, private security: SecurityService, private messageServ: MessageService) {
         this.sendRecipe();
+    }
+
+    private sendRecipe() {
+        this.select().subscribe({
+            next: (next) => {
+                next.forEach(r => {
+                    r.display = false;
+                    r.filterKeyWorkds = r.name + (r.keywords == undefined ? "" : " " + r.keywords);
+                });
+                this.recipesSubject$.next(next);
+            },
+            error: (error) => this.messageServ.add({ severity: 'error', summary: 'Erreur de base de donnée', detail: error })
+        })
     }
 
     public delRecipe(recipe: Recipe) {
-        this.recipes = this.recipes.filter(function (obj) {
-            return obj.name !== recipe.name;
+        this.delete(recipe).subscribe({
+            next: (next) => {
+                console.log('delete recipe :', next);
+                if (next.error) {
+                    this.messageServ.add({ severity: 'error', summary: 'Erreur de base de donnée', detail: next.error.code + " - " + next.error.status });
+                    return;
+                }
+                this.messageServ.add({ severity: 'warn', summary: 'Suppression', detail: 'Recette supprimée' });
+                this.sendRecipe();
+            },
+            error: (error) => this.messageServ.add({ severity: 'error', summary: 'Erreur de base de donnée', detail: error })
         });
-        this.messageServ.add({ severity: 'warn', summary: 'Suppression', detail: 'Recette supprimée' });
-        this.sendRecipe();
     }
 
     public insertRecipe(recipe: Recipe, callback: Function) {
-        recipe.id = 1;
-        this.recipes.push(recipe);
-        this.messageServ.add({ severity: 'info', summary: 'Création de la recette', detail: recipe.name });
-        callback();
-        this.sendRecipe();
+        this.insert(recipe).subscribe({
+            next: (next) => {
+                console.log('insert recipe :', next);
+                if (next.error) {
+                    this.messageServ.add({ severity: 'error', summary: 'Erreur de base de donnée', detail: next.error.code + " - " + next.error.status });
+                    return;
+                }
+                this.messageServ.add({ severity: 'info', summary: 'Création de la recette', detail: recipe.name });
+                callback();
+                this.sendRecipe();
+            },
+            error: (error) => this.messageServ.add({ severity: 'error', summary: 'Erreur de base de donnée', detail: error })
+        });
     }
 
     public updateRecipe(recipe: Recipe, callback: Function) {
-        this.recipes = this.recipes.filter(function (obj) {
-            return obj.name !== recipe.name;
+        // suppression des attributs liés à l'affichage
+        delete recipe['display'];
+        delete recipe['filterKeyWorkds'];
+
+        this.update(recipe).subscribe({
+            next: (next) => {
+                console.log('update recipe :', next);
+                if (next.error) {
+                    this.messageServ.add({ severity: 'error', summary: 'Erreur de base de donnée', detail: next.error.code + " - " + next.error.status });
+                    return;
+                }
+                this.messageServ.add({ severity: 'info', summary: 'Mise à jour de la recette', detail: recipe.name });
+                callback();
+                this.sendRecipe();
+            },
+            error: (error) => this.messageServ.add({ severity: 'error', summary: 'Erreur de base de donnée', detail: error })
         });
-        this.recipes.push(recipe);
-        this.messageServ.add({ severity: 'info', summary: 'Mise à jour de la recette', detail: recipe.name });
-        callback();
-        this.sendRecipe();
     }
 
+    private select() { return this.httpClient.get<Recipe[]>(this.apiRecipePath + '?by=name', { headers: this.security.getAppTocken() }) }
+    private insert(recipe: Recipe) { return this.httpClient.post<ApiResponse>(this.apiRecipePath, recipe, { headers: this.security.getAppTocken() }) }
+    private update(recipe: Recipe) { return this.httpClient.put<ApiResponse>(this.apiRecipePath + recipe.id, recipe, { headers: this.security.getAppTocken() }) }
+    private delete(recipe: Recipe) { return this.httpClient.delete<ApiResponse>(this.apiRecipePath + recipe.id, { headers: this.security.getAppTocken() }) }
+
+    public getRecipes(): Observable<Recipe[]> { return this.recipesSubject$.asObservable(); }
 }
